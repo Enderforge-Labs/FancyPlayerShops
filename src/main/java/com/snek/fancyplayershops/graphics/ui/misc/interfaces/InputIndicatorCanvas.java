26b37b94d397db04a2e094b99b25c4fbf6848cbb
UwU_Snek <thatsnekuwu@gmail.com>
2025-12-09 15:56:43 +0100

    Minor changes

--- src/main/java/com/snek/fancyplayershops/graphics/ui/misc/interfaces/InputIndicatorCanvas.java
+++ src/main/java/com/snek/fancyplayershops/graphics/ui/misc/interfaces/InputIndicatorCanvas.java
@@ -11,7 +11,7 @@ import com.snek.fancyplayershops.graphics.ui.misc.elements.InputIndicator;
 
 
 
-
+//TODO move to graphics.misc or something. check the other interfaces too
 public interface InputIndicatorCanvas {
     public @NotNull InputIndicator getLmbIndicator();
     public @NotNull InputIndicator getRmbIndicator();
