da8c2f06c3ac35f150ea32794117610ecb1cfb0e
UwU_Snek <thatsnekuwu@gmail.com>
2025-12-13 19:26:00 +0100

    Replace workaround in InputIndicator with proper logic

--- src/main/java/com/snek/fancyplayershops/graphics/ui/misc/elements/Any_InputIndicator.java
+++ src/main/java/com/snek/fancyplayershops/graphics/ui/misc/elements/Any_InputIndicator.java
@@ -3,13 +3,12 @@ package com.snek.fancyplayershops.graphics.ui.misc.elements;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector2f;
+import org.joml.Vector3d;
 
 import com.snek.fancyplayershops.main.Shop;
 import com.snek.fancyplayershops.graphics.ui.misc.styles.Any_InputIndicator_Text_S;
 import com.snek.fancyplayershops.graphics.ui.misc.styles.Any_InputIndicator_MouseButtonDown_S;
 import com.snek.fancyplayershops.graphics.ui.misc.styles.Any_InputIndicator_MouseButtonUp_S;
-import com.snek.frameworklib.data_types.animations.Transform;
-import com.snek.frameworklib.data_types.animations.Transition;
 import com.snek.frameworklib.data_types.graphics.AlignmentX;
 import com.snek.frameworklib.data_types.graphics.AlignmentY;
 import com.snek.frameworklib.data_types.graphics.TextAlignment;
@@ -34,14 +33,7 @@ public class Any_InputIndicator extends ShopDiv {
     public static final @NotNull Vector2f BUTTON_SIZE = new Vector2f(0.5f, 0.4f);   // The size of a mouse button compared to the mouse display
     public static final @NotNull Vector2f MOUSE_SIZE  = new Vector2f(0.025f, 1f);   // The size of the mouse display compared to the InputIndicator
     public static final float BUTTON_TEXT_SPACING = 0.025f / 2f;                    // The distance between mouse display and text
-
-    public static final float HIDDEN_TRANSLATION = 1000f;                           // The distance elements are moved by in order to hide them
-    private static final @NotNull Transition hideAnimation = new Transition().additiveTransform(new Transform().moveY(-HIDDEN_TRANSLATION));
-    private static final @NotNull Transition showAnimation = new Transition().additiveTransform(new Transform().moveY(+HIDDEN_TRANSLATION));
-
-
     private final @NotNull ShopTextElm text;
-    private boolean isVisible = true;
 
 
 
@@ -74,10 +66,14 @@ public class Any_InputIndicator extends ShopDiv {
         e.setAlignment(AlignmentX.RIGHT, AlignmentY.BOTTOM);
         ((Elm)e).getStyle(SimpleTextElmStyle.class).setTextAlignment(TextAlignment.LEFT);
         text = (ShopTextElm)e;
+    }
 
 
-        // Manually prime animation if necessary
-        updateDisplay(null);
+
+
+    @Override
+    public void spawn(@NotNull Vector3d pos, boolean animate) {
+        // Empty
     }
 
 
@@ -92,18 +88,12 @@ public class Any_InputIndicator extends ShopDiv {
         // If the description is not null and the display is hidden, show it and update the text
         if(description != null) {
             text.getStyle(SimpleTextElmStyle.class).setText(new Txt(description).lightGray().get());
-            if(!isVisible) {
-                applyAnimationNowRecursive(showAnimation);
-                isVisible = true;
-            }
+            super.spawn(canvas.getContext().getSpawnPos(), false);
         }
 
         // If the description is null and the display is visible, hide it.
         else {
-            if(isVisible) {
-                applyAnimationNowRecursive(hideAnimation);
-                isVisible = false;
-            }
+            super.despawn(false);
         }
     }
 }
