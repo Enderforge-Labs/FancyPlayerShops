5afbf70271881c4f79173557e7efce2408889a4b
UwU_Snek <thatsnekuwu@gmail.com>
2025-12-13 03:41:17 +0100

    Fix default colors of list scrollbars

--- src/main/java/com/snek/fancyplayershops/graphics/__default_ScrollableListThumbStyle.java
+++ src/main/java/com/snek/fancyplayershops/graphics/__default_ScrollableListThumbStyle.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3i;
 
 import com.snek.frameworklib.graphics.basic.styles.PanelElmStyle;
+import com.snek.frameworklib.graphics.core.styles.CanvasBorder_S;
 
 
 
@@ -25,11 +26,11 @@ public class __default_ScrollableListThumbStyle extends PanelElmStyle {
 
     @Override
     public @NotNull Vector3i getDefaultColor() {
-        return new Vector3i(255, 0, 0);
-    } //TODO choose default colors
+        return new Vector3i(180);
+    }
 
     @Override
     public int getDefaultAlpha() {
         return 255;
-    } //TODO choose default colors
+    }
 }
