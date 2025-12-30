64be19a2f0f517f6455d64b9d0015ffe70785a26
UwU_Snek <thatsnekuwu@gmail.com>
2025-12-30 01:51:02 +0100

    Add giveItem method to StashManager Improve other StashManager methods

--- src/main/java/com/snek/fancyplayershops/data/StashManager.java
+++ src/main/java/com/snek/fancyplayershops/data/StashManager.java
@@ -98,11 +98,15 @@ public final class StashManager extends UtilityClassBase {
      * @param count The amount of items to add.
      */
     public static void stashItem(final @NotNull UUID playerUUID, final @NotNull UUID itemUUID, final @NotNull ItemStack item, final int count) {
+        if(count == 0) return;
         if(item.is(Items.AIR)) return;
         final PlayerStash stash = stashes.computeIfAbsent(playerUUID, k -> new PlayerStash());
         final StashEntry stashEntry = stash.computeIfAbsent(itemUUID, k -> new StashEntry(item));
         stashEntry.add(count);
+        StashManager.scheduleStashSave(playerUUID);
     }
+
+
     /**
      * Adds an item with known UUID to the stash of the specified player.
      * <p> Air is never stashed.
@@ -110,21 +114,51 @@ public final class StashManager extends UtilityClassBase {
      * @param itemUUID The UUID of the item.
      * @param item The item to add.
      * @param count The amount of items to add.
+     * @param playerFeedback Whether to send the player a feedback message.
      */
-    public static void stashItem(final @NotNull UUID playerUUID, final @NotNull ItemStack item, final int count) {
+    public static void stashItem(final @NotNull UUID playerUUID, final @NotNull ItemStack item, final int count, final boolean playerFeedback) {
         if(count == 0) return;
         if(item.is(Items.AIR)) return;
         stashItem(playerUUID, MinecraftUtils.calcItemUUID(item), item, count);
 
         // Send feedback to player
-        final @Nullable Player player = MinecraftUtils.getPlayerByUUID(playerUUID);
-        if(player != null) {
-            player.displayClientMessage(new Txt()
-                .cat(Utils.formatAmount(count, false, true) + "x ")
-                .cat(MinecraftUtils.getFancyItemName(item).getString())
-                .cat(" " + (count == 1 ? "has" : "have") + " been sent to your stash.")
-            .lightGray().get(), false);
+        if(playerFeedback) {
+            final @Nullable Player player = MinecraftUtils.getPlayerByUUID(playerUUID);
+            if(player != null) {
+                player.displayClientMessage(new Txt()
+                    .cat(Utils.formatAmount(count, false, true) + "x ")
+                    .cat(MinecraftUtils.getFancyItemName(item).getString())
+                    .cat(" " + (count == 1 ? "has" : "have") + " been sent to your stash.")
+                .lightGray().get(), false);
+            }
         }
+    }
+
+
+    /**
+     * Gives the specified player {@code amount} items.
+     * <p>
+     * Items that don't fit in the inventory are sent to the player's stash.
+     * @param player The player to give items to.
+     * @param item The item to give to the player.
+     * @param amount The number of items to give.
+     * @param playerFeedback Whether to send the player a feedback message.
+     * @return A pair of integers representing the amount of items sent to the player's inventory and the amount of items sent to their stash.
+     */
+    public static @NotNull Pair<Integer, Integer> giveItem(final @NotNull Player player, final @NotNull ItemStack item, final int amount, final boolean playerFeedback) {
+
+        // Try to put items in the inventory
+        final ItemStack _item = item.copyWithCount(amount);
+        MinecraftUtils.attemptGive(player, _item);
+        final int stashedAmount = _item.getCount();
+
+        // Send remaining items to the stash
+        if(stashedAmount > 0) {
+            StashManager.stashItem(player.getUUID(), _item, _item.getCount(), playerFeedback);
+        }
+
+        // Return stats
+        return Pair.from(amount - stashedAmount, stashedAmount);
     }
 
 
