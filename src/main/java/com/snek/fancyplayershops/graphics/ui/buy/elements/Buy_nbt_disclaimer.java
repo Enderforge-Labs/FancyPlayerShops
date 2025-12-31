437bda62117f031a330cae1c082cdd1be25ed69c
UwU_Snek <thatsnekuwu@gmail.com>
2025-12-31 17:00:53 +0100

    Fix hover animation of nbt disclaimer button

--- src/main/java/com/snek/fancyplayershops/graphics/ui/buy/elements/Buy_nbt_disclaimer.java
+++ src/main/java/com/snek/fancyplayershops/graphics/ui/buy/elements/Buy_nbt_disclaimer.java
@@ -1,14 +1,13 @@
 package com.snek.fancyplayershops.graphics.ui.buy.elements;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.joml.Vector2f;
 
-import com.snek.fancyplayershops.graphics.ui.buy.BuyCanvas;
 import com.snek.fancyplayershops.graphics.ui.buy.styles.Buy_nbt_disclaimer_S;
 import com.snek.fancyplayershops.main.FancyPlayerShops;
 import com.snek.frameworklib.graphics.functional.elements.FancyButtonElm;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickAction;
@@ -33,7 +32,7 @@ public class Buy_nbt_disclaimer extends FancyButtonElm {
     @Override
     public void onClick(final @NotNull Player player, final @NotNull ClickAction click, final @NotNull Vector2f coords) {
         super.onClick(player, click, coords);
-
+        player.displayClientMessage(Component.literal("test messge"), false);
         //TODO open Mixed nbt disclaimer
     }
 }
--- src/main/java/com/snek/fancyplayershops/graphics/ui/buy/styles/Buy_nbt_disclaimer_S.java
+++ src/main/java/com/snek/fancyplayershops/graphics/ui/buy/styles/Buy_nbt_disclaimer_S.java
@@ -1,9 +1,14 @@
 package com.snek.fancyplayershops.graphics.ui.buy.styles;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3i;
 
+import com.snek.frameworklib.data_types.animations.Animation;
+import com.snek.frameworklib.data_types.animations.Transition;
 import com.snek.frameworklib.graphics.functional.styles.FancyButtonElmStyle;
+import com.snek.frameworklib.graphics.functional.styles.__base_ButtonElmStyle;
+import com.snek.frameworklib.utils.Easings;
 import com.snek.frameworklib.utils.Txt;
 
 import net.minecraft.network.chat.Component;
@@ -12,6 +17,10 @@ import net.minecraft.network.chat.Component;
 
 
 public class Buy_nbt_disclaimer_S extends FancyButtonElmStyle {
+    public static final Vector3i DEFAULT_COLOR = new Vector3i(255, 0, 0);
+    public static final Vector3i HOVERED_COLOR = new Vector3i(255, 64, 64);
+
+
     public Buy_nbt_disclaimer_S() {
         super();
     }
@@ -19,18 +28,46 @@ public class Buy_nbt_disclaimer_S extends FancyButtonElmStyle {
 
     @Override
     public @NotNull Vector3i getDefaultBgColor() {
-        return new Vector3i(255, 0, 0);
+        return new Vector3i(DEFAULT_COLOR);
     }
 
 
     @Override
     public int getDefaultBgAlpha() {
-        return 96;
+        return 32;
     }
 
 
     @Override
     public @NotNull Component getDefaultText() {
-        return new Txt("MIXED NBTs").red().get();
+            return new Txt("⚠ Mixed NBTs ⚠").red().get();
     }
+
+
+    @Override
+    public @Nullable Animation getDefaultHoverPrimerAnimation() {
+        return null;
+    }
+
+    @Override
+    public @Nullable Animation getDefaultHoverEnterAnimation() {
+        return new Animation(
+            new Transition(__base_ButtonElmStyle.HOVER_ANIMATION_TIME, Easings.expOut)
+            .targetBgColor(HOVERED_COLOR)
+        );
+    }
+
+    @Override
+    public @Nullable Animation getDefaultHoverLeaveAnimation() {
+        return new Animation(
+            new Transition(__base_ButtonElmStyle.HOVER_ANIMATION_TIME, Easings.expOut)
+            .targetBgColor(DEFAULT_COLOR)
+        );
+    }
+
+    @Override
+    public @Nullable Animation getDefaultHoverInversePrimerAnimation() {
+        return null;
+    }
+
 }
