2c4f4016c42e11ab800ede4c8c357d9bb9262313
UwU_Snek <thatsnekuwu@gmail.com>
2026-01-01 22:17:32 +0100

    Code cleanup

--- src/main/java/com/snek/fancyplayershops/graphics/hud/_mainmenu_/_MainMenuCanvas.java
+++ src/main/java/com/snek/fancyplayershops/graphics/hud/_mainmenu_/_MainMenuCanvas.java
@@ -1,12 +1,9 @@
 package com.snek.fancyplayershops.graphics.hud._mainmenu_;
 
-import java.util.Set;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.joml.Vector2f;
 
-import com.snek.fancyplayershops.data.ProductDisplayManager;
 import com.snek.fancyplayershops.graphics.hud._mainmenu_.elements._MainMenu_BrowseShopsButton;
 import com.snek.fancyplayershops.graphics.hud._mainmenu_.elements._MainMenu_InfoButton;
 import com.snek.fancyplayershops.graphics.hud._mainmenu_.elements._MainMenu_ManageShopsButton;
@@ -15,14 +12,11 @@ import com.snek.fancyplayershops.graphics.hud._mainmenu_.elements._MainMenu_Pref
 import com.snek.fancyplayershops.graphics.hud._mainmenu_.elements._MainMenu_RecentActionsButton;
 import com.snek.fancyplayershops.graphics.hud._mainmenu_.elements._MainMenu_StatsButton;
 import com.snek.fancyplayershops.graphics.hud.core.elements.HudCanvasBase;
-import com.snek.fancyplayershops.graphics.hud.mainmenu.styles.MainMenu_EmptyText_S;
 import com.snek.fancyplayershops.graphics.hud.misc.elements.Hud_CloseButton;
 import com.snek.fancyplayershops.graphics.misc.elements.TitleElm;
 import com.snek.fancyplayershops.main.FancyPlayerShops;
-import com.snek.fancyplayershops.main.ProductDisplay;
 import com.snek.frameworklib.data_types.graphics.AlignmentX;
 import com.snek.frameworklib.data_types.graphics.AlignmentY;
-import com.snek.frameworklib.graphics.basic.elements.SimpleTextElm;
 import com.snek.frameworklib.graphics.core.HudContext;
 import com.snek.frameworklib.graphics.layout.Div;
 import com.snek.frameworklib.graphics.layout.HoverableDiv;
@@ -81,51 +75,6 @@ public class _MainMenuCanvas extends HudCanvasBase {
             e.setSize(buttonSize);
             e.setAlignment(AlignmentX.RIGHT, AlignmentY.BOTTOM);
         }
-
-
-        // // Add no products text if the player doesn't own any product display
-        // final @Nullable Set<@NotNull ProductDisplay> displays = ProductDisplayManager.getDisplaysOfPlayer(player);
-        // if(displays == null || displays.isEmpty()) {
-        //     e = bg.addChild(new SimpleTextElm(level, new MainMenu_EmptyText_S()));
-        //     e.setSize(new Vector2f(1f, FancyPlayerShops.LINE_H));
-        //     e.setAlignment(AlignmentX.CENTER, AlignmentY.CENTER);
-        // }
-
-
-        // // If displays are present
-        // else {
-
-        //     // Add shop header
-        //     final List<Shop> shops = ShopManager.getShops(player);
-        //     e = bg.addChild(new MainMenu_ShopHeader(_hud, shops));
-        //     e.setSize(new Vector2f(1f, HEADER_H));
-        //     e.setAlignmentX(AlignmentX.LEFT);
-        //     e.setPosY(1f - FancyPlayerShops.LINE_H - HEADER_H);
-
-        //     // Create scrollable list
-        //     final float list_elm_h = 1f / LIST_SIZE;
-        //     list = (ScrollableList)bg.addChild(new ScrollableList(level, list_elm_h));
-        //     list.setSize(new Vector2f(1f, LIST_H));
-        //     list.setAlignmentX(AlignmentX.LEFT);
-        //     list.setPosY(FancyPlayerShops.SQUARE_BUTTON_SIZE);
-
-
-        //     // Add shop entry displays
-        //     for(int i = 0; i < shops.size(); ++i) {
-        //        list.storeElm(new MainMenu_ShopEntry(_hud, shops.get(i), list));
-        //     }
-        // }
-
-
-        // // Add close button
-        // e = bg.addChild(new Hud_CloseButton(_hud));
-        // e.setSize(new Vector2f(FancyPlayerShops.LINE_H));
-        // e.setAlignment(AlignmentX.LEFT, AlignmentY.TOP);
-
-        // // Add info button
-        // e = bg.addChild(new _MainMenu_InfoButton(_hud));
-        // e.setSize(new Vector2f(FancyPlayerShops.LINE_H));
-        // e.setAlignment(AlignmentX.RIGHT, AlignmentY.TOP);
 
 
         // Add bottom bar buttons
